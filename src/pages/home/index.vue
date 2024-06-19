ec3329f23135a4efc2ec9385d008c810eedb2a56
Johannes Daniswara Pratama <115395772+kisenaa@users.noreply.github.com>
2024-06-19 11:02:25 +0700

    test: husky hooks

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -6,6 +6,6 @@ defineOptions({
 
 <template>
   <div class="h-screen flex justify-center items-center">
-    <div class="text-blue-600 text-6xl">Homepage / Landing page here</div>
+    <div class="text-blue-600 text-6xl">Homepage / Landing page here yeah</div>
   </div>
 </template>
