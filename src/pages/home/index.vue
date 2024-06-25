e0d0540525dfd3a89335bab0770eddfcaca98644
Rafli <raflidz24@gmail.com>
2024-06-25 21:48:40 +0700

    homepage, cosmetic only

--- src/pages/home/index.vue
+++ src/pages/home/index.vue
@@ -5,7 +5,422 @@ defineOptions({
 </script>
 
 <template>
-  <div class="flex h-screen items-center justify-center">
-    <div class="text-6xl text-blue-600">Homepage / Landing page here yeah</div>
+  <div class="bg-black/opacity-20 relative h-[2373px] w-[1440px]">
+    <div class="absolute left-[271px] top-[384px] size-px"></div>
+    <div class="absolute left-0 top-[1830px] h-[606px] w-[1440px]">
+      <div
+        class="absolute left-0 top-0 h-[606px] w-[1440px] bg-neutral-900"
+      ></div>
+      <div
+        class="absolute left-0 top-[2px] inline-flex h-[604px] w-[1440px] flex-col items-center justify-center gap-2.5 bg-neutral-900 px-[120px] py-[100px]"
+      >
+        <div
+          class="flex h-[396.52px] flex-col items-center justify-center gap-[47px]"
+        >
+          <div class="inline-flex items-end justify-start gap-[61px]">
+            <div class="flex items-start justify-start gap-[138px]">
+              <div
+                class="inline-flex flex-col items-start justify-start gap-[15px]"
+              >
+                <div
+                  class="font-['Inter'] text-base font-bold leading-7 text-white"
+                >
+                  Product
+                </div>
+                <div class="flex flex-col items-start justify-start gap-[13px]">
+                  <div
+                    class="font-['Inter'] text-base font-normal text-white opacity-70"
+                  >
+                    Diam orci
+                  </div>
+                  <div
+                    class="font-['Inter'] text-base font-normal text-white opacity-70"
+                  >
+                    Mi feugiat
+                  </div>
+                  <div
+                    class="font-['Inter'] text-base font-normal text-white opacity-70"
+                  >
+                    Netus fermentum
+                  </div>
+                  <div
+                    class="font-['Inter'] text-base font-normal text-white opacity-70"
+                  >
+                    Suspendisse viverra
+                  </div>
+                  <div
+                    class="font-['Inter'] text-base font-normal text-white opacity-70"
+                  >
+                    Id dolor
+                  </div>
+                  <div
+                    class="font-['Inter'] text-base font-normal text-white opacity-70"
+                  >
+                    Erat mattis
+                  </div>
+                </div>
+              </div>
+              <div
+                class="inline-flex flex-col items-start justify-start gap-[15px]"
+              >
+                <div
+                  class="font-['Inter'] text-base font-bold leading-7 text-white"
+                >
+                  Information
+                </div>
+                <div class="flex flex-col items-start justify-start gap-[13px]">
+                  <div
+                    class="font-['Inter'] text-base font-normal text-white opacity-70"
+                  >
+                    Nibh
+                  </div>
+                  <div
+                    class="font-['Inter'] text-base font-normal text-white opacity-70"
+                  >
+                    Egestas
+                  </div>
+                  <div
+                    class="font-['Inter'] text-base font-normal text-white opacity-70"
+                  >
+                    Dictum
+                  </div>
+                </div>
+              </div>
+              <div
+                class="inline-flex flex-col items-start justify-start gap-[15px]"
+              >
+                <div
+                  class="w-[159px] font-['Inter'] text-base font-bold leading-7 text-white"
+                >
+                  Company
+                </div>
+                <div
+                  class="flex h-[115px] w-[159px] flex-col items-start justify-start gap-3"
+                >
+                  <div
+                    class="self-stretch font-['Inter'] text-base font-normal text-white opacity-70"
+                  >
+                    Id maecenas
+                  </div>
+                  <div
+                    class="self-stretch font-['Inter'] text-base font-normal text-white opacity-70"
+                  >
+                    Id orci
+                  </div>
+                  <div
+                    class="self-stretch font-['Inter'] text-base font-normal text-white opacity-70"
+                  >
+                    Magna ultricies
+                  </div>
+                  <div
+                    class="self-stretch font-['Inter'] text-base font-normal text-white opacity-70"
+                  >
+                    Quis risus
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div
+              class="inline-flex flex-col items-start justify-start gap-2.5 rounded-[5px] bg-black px-[42px] py-[30px]"
+            >
+              <div class="flex flex-col items-start justify-start gap-[18px]">
+                <div
+                  class="w-[149px] font-['Inter'] text-base font-bold leading-7 text-white"
+                >
+                  Subscribe
+                </div>
+                <div
+                  class="inline-flex w-[248px] items-center justify-start gap-14 rounded-md border border-zinc-300 bg-white pl-[15px]"
+                >
+                  <div
+                    class="shrink grow basis-0 font-['Inter'] text-sm font-normal text-neutral-700 opacity-60"
+                  >
+                    Email address
+                  </div>
+                  <div
+                    class="inline-flex flex-col items-center justify-center rounded-r-md bg-blue-600 p-[13px]"
+                  >
+                    <div class="relative size-6">
+                      <img src="../../assets/image/arrowenter.png" alt="" />
+                    </div>
+                  </div>
+                </div>
+                <div
+                  class="h-[82px] w-[254px] font-['Inter'] text-xs font-normal leading-tight text-white opacity-70"
+                >
+                  Gravida sed justo, justo, id est et. Amet tristique convallis
+                  sed porttitor nullam eu ut. Duis et odio aliquam bibendum.
+                  Metus et lectus id viverra fringilla magna morbi.
+                </div>
+              </div>
+            </div>
+          </div>
+          <div
+            class="h-px self-stretch border border-white bg-white opacity-5"
+          ></div>
+          <div class="inline-flex items-center justify-between self-stretch">
+            <div class="relative h-[45.52px] w-[204.18px]">
+              <img src="../../assets/image/Frame.png" alt="" />
+            </div>
+            <div class="flex items-start justify-start gap-9">
+              <div class="font-['Inter'] text-sm font-medium text-white">
+                Terms
+              </div>
+              <div class="font-['Inter'] text-sm font-medium text-white">
+                Privacy
+              </div>
+              <div class="font-['Inter'] text-sm font-medium text-white">
+                Cookies
+              </div>
+            </div>
+            <div class="flex items-start justify-start gap-[15px]">
+              <div class="relative size-[35px]">
+                <div class="absolute left-[8px] top-[7px] size-5 border">
+                  <img src="../../assets/image/linkedin.png" alt="" />
+                </div>
+              </div>
+              <div class="relative size-[35px]">
+                <div class="absolute left-[8px] top-[7px] size-5">
+                  <img src="../../assets/image/face.png" alt="" />
+                </div>
+              </div>
+              <div class="relative size-[35px]">
+                <div class="absolute left-[8px] top-[8px] size-5">
+                  <img src="../../assets/image/IDK.png" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="absolute left-0 top-0 h-[91px] w-[1440px]">
+      <div
+        class="absolute left-0 top-0 h-[91px] w-[1440px] bg-neutral-900"
+      ></div>
+      <div
+        class="absolute left-[135px] top-[30px] inline-flex h-[30px] w-[134.57px] items-center justify-center gap-1.5"
+      >
+        <div class="relative h-[30px] w-[134.57px]">
+          <img src="../../assets/image/Frame.png" alt="" />
+        </div>
+      </div>
+      <div
+        class="absolute left-[940px] top-[35px] font-['Inter'] text-[15px] font-semibold leading-[15px] text-white"
+      >
+        Home
+      </div>
+      <div
+        class="absolute left-[1002.04px] top-[35px] font-['Inter'] text-[15px] font-semibold leading-[15px] text-white"
+      >
+        Testimony
+      </div>
+      <div
+        class="absolute left-[1101.04px] top-[35px] font-['Inter'] text-[15px] font-semibold leading-[15px] text-white"
+      >
+        Contact
+      </div>
+      <div
+        class="absolute left-[1197.04px] top-[21px] inline-flex h-[43px] w-[101px] items-center justify-start gap-2.5 rounded-3xl bg-blue-600 px-6 py-3.5"
+      >
+        <div
+          class="font-['Inter'] text-[15px] font-semibold leading-[15px] text-white"
+        >
+          Sign up
+        </div>
+      </div>
+    </div>
+    <div class="absolute left-0 top-[91px] h-[801px] w-[1440px]">
+      <div
+        class="absolute left-0 top-0 h-[801px] w-[1440px] bg-neutral-900"
+      ></div>
+      <div
+        class="absolute left-[446px] top-[465px] inline-flex h-[66px] w-[258px] items-center justify-start gap-2.5 rounded-[100px] bg-blue-600 py-6 pl-6 pr-[37px]"
+      >
+        <div
+          class="shrink grow basis-0 text-center font-['Inter'] text-2xl font-semibold text-white"
+        >
+          Get started
+        </div>
+      </div>
+      <div
+        class="absolute left-[147px] top-[465px] inline-flex h-[66px] w-[258px] items-center justify-start gap-2.5 rounded-[100px] bg-fuchsia-800 py-6 pl-6 pr-[37px]"
+      >
+        <div
+          class="shrink grow basis-0 text-center font-['Inter'] text-2xl font-semibold text-white"
+        >
+          Learn More<br />
+        </div>
+      </div>
+      <div class="absolute left-[147px] top-[152px] h-[254px] w-[591px]">
+        <div
+          class="absolute left-0 top-0 w-[591px] font-['Inter'] text-[53px] font-semibold leading-[58.30px] text-white"
+        >
+          Practicum Management System
+        </div>
+        <div
+          class="absolute left-0 top-[158px] w-[555px] font-['Inter'] text-xl font-normal leading-loose text-white"
+        >
+          Effortlessly manage schedule, rooms, fieldwork, and student placements
+          with our innovative and user-friendly platform. Come and Join us !
+        </div>
+      </div>
+      <div class="absolute left-[879px] top-[166px] h-[350.02px] w-[426.86px]">
+        <img src="../../assets/image/Illustration.png" alt="" />
+      </div>
+    </div>
+    <div class="absolute left-0 top-[887px] h-[943px] w-[1440px]">
+      <div
+        class="absolute left-0 top-0 h-[943px] w-[1440px] bg-neutral-900"
+      ></div>
+      <div class="absolute left-[735px] top-[337px] h-[215px] w-[570px]">
+        <div
+          class="absolute left-0 top-0 h-[215px] w-[570px] rounded-3xl border border-white"
+        ></div>
+        <div class="absolute left-[239px] top-[52px] h-[111px] w-[298px]">
+          <div
+            class="absolute left-0 top-0 font-['Inter'] text-xl font-semibold leading-tight text-white"
+          >
+            Practicum List
+          </div>
+          <div
+            class="absolute left-0 top-[27px] w-[298px] font-['Inter'] text-base font-normal leading-relaxed text-white"
+          >
+            Muffin lemon drops toffee. Pudding tootsie roll brownie jelly beans.
+          </div>
+          <div
+            class="absolute left-0 top-[91px] inline-flex h-5 w-[108px] items-start justify-start"
+          >
+            <div
+              class="font-['Inter'] text-base font-semibold leading-none text-white"
+            >
+              Learn more
+            </div>
+            <div class="relative size-5">
+              <div class="absolute left-[5px] top-[6px] h-[8.75px] w-2.5"></div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="absolute left-[32px] top-[26px] size-[167px] rounded-3xl bg-indigo-50"
+        >
+          <div class="absolute left-0 top-0 size-[167px]">
+            <img src="../../assets/image/prakticumlist.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div class="absolute left-[735px] top-[582px] h-[215px] w-[570px]">
+        <div
+          class="absolute left-0 top-0 h-[215px] w-[570px] rounded-3xl border border-white"
+        ></div>
+        <div class="absolute left-[239px] top-[53px] h-[109px] w-[298px]">
+          <div
+            class="absolute left-0 top-0 font-['Inter'] text-xl font-semibold leading-tight text-white"
+          >
+            Overview Report
+          </div>
+          <div
+            class="absolute left-0 top-[25px] w-[298px] font-['Inter'] text-base font-normal leading-relaxed text-white"
+          >
+            Muffin lemon drops toffee. Pudding tootsie roll brownie jelly beans.
+          </div>
+          <div
+            class="absolute left-0 top-[89px] inline-flex h-5 w-[108px] items-center justify-center"
+          >
+            <div
+              class="font-['Inter'] text-base font-semibold leading-none text-white"
+            >
+              Learn more
+            </div>
+            <div
+              class="relative flex size-5 flex-col items-start justify-start"
+            >
+              <div class="relative h-[8.75px] w-2.5"></div>
+            </div>
+          </div>
+        </div>
+        <div
+          class="absolute left-[32px] top-[26px] size-[167px] rounded-3xl bg-rose-50"
+        >
+          <div class="absolute left-0 top-0 size-[167px]">
+            <img src="../../assets/image/overviewreport.png" alt="" />
+          </div>
+        </div>
+      </div>
+      <div class="absolute left-[135px] top-[582px] h-[215px] w-[570px]">
+        <div
+          class="absolute left-[32px] top-[26px] size-[167px] rounded-3xl bg-orange-50"
+        >
+          <div class="absolute left-0 top-0 size-[167px]">
+            <img src="../../assets/image/roomplan.png" alt="" srcset="" />
+          </div>
+        </div>
+        <div class="absolute left-[239px] top-[52px] h-[111px] w-[298px]">
+          <div
+            class="absolute left-0 top-0 font-['Inter'] text-xl font-semibold leading-tight text-white"
+          >
+            Room Planning
+          </div>
+          <div
+            class="absolute left-0 top-[27px] w-[298px] font-['Inter'] text-base font-normal leading-relaxed text-white"
+          >
+            Muffin lemon drops toffee. Pudding tootsie roll brownie jelly beans.
+          </div>
+          <div
+            class="absolute left-0 top-[91px] inline-flex h-5 w-[108px] items-start justify-start"
+          >
+            <div
+              class="font-['Inter'] text-base font-semibold leading-none text-white"
+            >
+              Learn more
+            </div>
+            <div class="relative size-5">
+              <div class="absolute left-[5px] top-[6px] h-[8.75px] w-2.5"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class="absolute left-[135px] top-[337px] h-[215px] w-[570px]">
+        <div
+          class="absolute left-0 top-0 h-[215px] w-[570px] rounded-3xl border border-white bg-neutral-900"
+        ></div>
+        <div
+          class="absolute left-[32px] top-[26px] size-[167px] rounded-3xl bg-sky-50"
+        >
+          <div class="absolute left-0 top-0 size-[167px]">
+            <img src="../../assets/image/easysched.png" alt="" />
+          </div>
+        </div>
+        <div class="absolute left-[239px] top-[52px] h-[111px] w-[298px]">
+          <div
+            class="absolute left-0 top-0 font-['Inter'] text-xl font-semibold leading-tight text-white"
+          >
+            Easy Schedule
+          </div>
+          <div
+            class="absolute left-0 top-[27px] w-[298px] font-['Inter'] text-base font-normal leading-relaxed text-white"
+          >
+            Muffin lemon drops toffee. Pudding tootsie roll brownie jelly beans.
+          </div>
+          <div
+            class="absolute left-0 top-[91px] inline-flex h-5 w-[108px] items-start justify-start"
+          >
+            <div
+              class="font-['Inter'] text-base font-semibold leading-none text-white"
+            >
+              Learn more
+            </div>
+            <div class="relative size-5">
+              <div class="absolute left-[5px] top-[6px] h-[8.75px] w-2.5"></div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div
+        class="absolute left-[306px] top-[119px] w-[852px] text-center font-['Inter'] text-5xl font-semibold leading-[57.60px] text-white"
+      >
+        Our Application Features
+      </div>
+    </div>
   </div>
 </template>
