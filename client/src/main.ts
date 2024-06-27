ce36312c59983b235a771c8201a314cf825c4756
Johannes Daniswara Pratama <115395772+kisenaa@users.noreply.github.com>
2024-06-27 08:21:20 +0700

    fix: localstorage not saving auth info

--- client/src/main.ts
+++ client/src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue';
 // Vue Router
 import { createPinia } from 'pinia';
-// import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate'
 import { router } from './router';
 
 import App from '~/App.vue';
@@ -11,7 +11,7 @@ import './styles/index.css';
 
 const app = createApp(App);
 const pinia = createPinia()
-// pinia.use(piniaPluginPersistedstate)
+pinia.use(piniaPluginPersistedstate)
 
 app.use(pinia);
 app.use(router);
